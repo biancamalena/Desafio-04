cb6ea6389a7be5a37a20b4028eb68d792e365309
trizzzg <beatriz.garreto@gmail.com>
2025-05-18 16:08:53 -0300

    Cenario

--- Assets/Scripts/Fundo.cs
+++ Assets/Scripts/Fundo.cs
@@ -6,38 +6,22 @@ public class Fundo : MonoBehaviour
     private float larguraTotal;
     private Vector3 posInicial;
 
-    void Start()
+    void Awake()
     {
-        posInicial = transform.position;
-
-        // Calcula a largura total somando os bounds dos filhos
-        SpriteRenderer[] sprites = GetComponentsInChildren<SpriteRenderer>();
-        float minX = float.MaxValue;
-        float maxX = float.MinValue;
-
-        foreach (SpriteRenderer sr in sprites)
-        {
-            float left = sr.bounds.min.x;
-            float right = sr.bounds.max.x;
-
-            if (left < minX) minX = left;
-            if (right > maxX) maxX = right;
-        }
-
-        larguraTotal = maxX - minX;
+        this.posInicial = transform.position;
+        this.larguraTotal = GetComponentInChildren<SpriteRenderer>().size.x;
+       
     }
 
     void Update()
     {
-        // Move para a esquerda
-        transform.Translate(Vector2.left * velocidade * Time.deltaTime);
+        // Move o fundo para a esquerda
+        transform.Translate(Vector3.left * velocidade * Time.deltaTime);
 
-        // Reposiciona o fundo se ele saiu da tela
+        // Quando ele sair totalmente da tela, volta pro ponto inicial + largura * 2 (pra continuar o loop)
         if (transform.position.x < posInicial.x - larguraTotal)
         {
-            transform.position += Vector3.right * larguraTotal * 2f;
+            transform.position = new Vector3(posInicial.x, transform.position.y, transform.position.z);
         }
     }
 }
-
-
